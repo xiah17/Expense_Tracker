5187d66f5138b0009c711af26704eab4b829cea9
xiah17 <Jalen.1720@gmail.com>
2025-03-22 20:36:54 +0800

    new added code

--- obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8af71a3db802f6526e003756a6eeaac8d9bcce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
