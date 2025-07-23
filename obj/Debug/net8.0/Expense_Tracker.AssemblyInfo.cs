019f8a8fcf2307f633c5f35260019efdb47b6a5b
xiah17 <Jalen.1720@gmail.com>
2025-07-23 20:12:28 +0800

    Category Controller

--- obj/Debug/net8.0/Expense_Tracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/Expense_Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense_Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53753690bcc3ddde5f4aa8a7c3d228110cef5da4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afa955e620073ec4526bb088e9665537eec704f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense_Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense_Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
